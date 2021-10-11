aabb39b335c7aed2706c4678074261d81c1495c9
tudemaha <gede_mahardika88@yahoo.co.id>
2021-10-11 17:51:54 +0800

    Create .gitignore and edit StandarDeviasiSample.c (int to double)

--- StandarDeviasiSample/StandarDeviasiSample.c
+++ StandarDeviasiSample/StandarDeviasiSample.c
@@ -2,36 +2,43 @@
 #include <math.h>
 
 int main() {
+    // cetak judul
     printf("========== STANDAR DEVIASI SAMPLE ==========\n");
     printf("============================================\n");
 
-    int size;
+    int size;                   // deklarasi size array untuk banyak data
     printf("Banyak data: ");
-    scanf("%d", &size);
+    scanf("%d", &size);         // terima input size
 
-    int dataArray[size];
+    // buat array dengan panjang size untuk menampung data
+    double dataArray[size];
 
+    // for loop untuk menerima input data sebanyak size
     for(int i = 0; i < size; i++) {
-        printf("Masukkan data ke-%d: ", size);
-        scanf("%lf", dataArray[i]);
+        printf("Masukkan data ke-%d: ", (i + 1));
+        scanf("%lf", &dataArray[i]);
     }
 
+    // jumlah semua data yang diinput
     double sum;
     for(int i = 0; i < size; i++) {
         sum += dataArray[i];
     }
 
+    // rata-rata data
     double average = sum / size;
 
-
+    // deklarasi kurang dan kuadrat
     double kurang, kuadrat;
-    for(int i = 1; i < size; i++) {
-        kurang = dataArray[i] - average;
-        kuadrat += (kurang * kurang);
+    for(int i = 0; i < size; i++) {
+        kurang = dataArray[i] - average;    // pengurangan data yang diinput dengan rata-rata
+        kuadrat += (kurang * kurang);       // menjumlah (sigma) semua hasil yang dikurangi ke dalam variable kuadrat
     }
 
+    // menghitung standar deviasi
     double deviasi = sqrt(kuadrat / (size - 1));
 
+    // cetak standar deviasi sample
     printf("Standar deviasi sample: %.2f\n", deviasi);
 
     return 0;
